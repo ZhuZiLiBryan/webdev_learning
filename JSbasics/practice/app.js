7c93e996d7c6d25833708f324c04c1f3f36fc7b6
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2024-07-17 18:30:24 -0700

    javascript functions:

--- JSbasics/practice/app.js
+++ JSbasics/practice/app.js
@@ -13,3 +13,43 @@ const myMath = {
         return a + b;
     }
 }
+
+const names = ["Josh", "Bob", "Amy"];
+
+const formattedNames = names.map( x => (
+    x.toUpperCase()
+))
+
+const numbers = [1, 2, 3, 4, 5];
+
+const even = numbers.filter( x => (   
+    x % 2 === 0 )
+)
+
+numbers.reduce((sum, num) => sum + num, 100)
+
+function d20(numSides = 20) {
+    return Math.floor(Math.random() * numSides) + 1
+}
+
+console.log(Math.max(...numbers))
+
+
+function printNames(...names) {
+    for (let name of names) {
+        console.log(`${name} `);
+    }
+}
+
+
+const book = {
+    title: "Great Gatsby",
+    isbn: 1723123123,
+    author: "Fitzgerald"
+}
+
+function bookInfo({title, isbn}) {
+    return `${title} ${isbn}`;
+}
+
+
