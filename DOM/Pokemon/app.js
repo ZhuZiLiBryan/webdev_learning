df10c01f237cbe3cb36ca261b1a3b4e3a75db54e
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2024-07-22 00:00:53 -0700

    inline listeners and arrow functions for element property

--- DOM/Pokemon/app.js
+++ DOM/Pokemon/app.js
@@ -1,9 +1,18 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
 let container = document.querySelector('#container');
+let uri = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`;
 
 for (let i = 1; i <= 151; i++) {
+    let entry = document.createElement('div');
     let pokeIcon = document.createElement('img');
-    pokeIcon.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
-    container.appendChild(pokeIcon);
+    let label = document.createElement('span');
+    label.innerText = `#${i}`;
+    pokeIcon.src = `${uri}${i}.png`;
+
+    entry.classList.add("pokemon");
+
+    entry.append(pokeIcon);
+    entry.append(label);
+    container.append(entry);
 }
