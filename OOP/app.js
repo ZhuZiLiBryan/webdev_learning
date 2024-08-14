3e21a7067ab328c1f4598a160b27319013af363d
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2024-08-14 10:49:26 -0700

    inheritance

--- OOP/app.js
+++ OOP/app.js
@@ -1,23 +1,54 @@
 // const navColor = new Color('carrot', [230, 126, 34]);
 // const logoColor = new Color('emerald', [46, 204, 113]);
 
-function makeColor(r, g, b) {
-    const color = {};
-    color.r = r;
-    color.g = g;
-    color.b = b;
+// function makeColor(r, g, b) {
+//     const color = {};
+//     color.r = r;
+//     color.g = g;
+//     color.b = b;
 
-    color.rgb = function () {
-        const { r, g, b } = this;
-        return `rgb(${r}, ${g}, ${b})`;
+//     color.rgb = function () {
+//         const { r, g, b } = this;
+//         return `rgb(${r}, ${g}, ${b})`;
+//     }
+
+//     color.hex = function () {
+//         const { r, g, b } = this;
+//         return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//     }
+//     return color;
+// }
+
+// const firstColor = makeColor(35, 255, 150);
+// firstColor.hex();
+
+class Pet {
+    constructor (name, age) {
+        this.name = name;
+        this.age = age;
     }
 
-    color.hex = function () {
-        const { r, g, b } = this;
-        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    eat() {
+        return `${this.name} is eating!`;
     }
-    return color;
 }
 
-const firstColor = makeColor(35, 255, 150);
-firstColor.hex();
+class Cat extends Pet {
+    constructor(name, age, livesLeft = 9) {
+        super(name, age);
+        this.livesLeft = livesLeft;
+    }
+    meow() {
+        return `MEOW!`;
+    }
+}
+
+class Dog extends Pet {
+    bark() {
+        return `WOOF!`;
+    }
+
+    eat() {
+        return `${this.name} scarfs its food!`;
+    }
+}
