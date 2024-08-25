47d09585238d53b81b999ca1462b418f13dfb2bc
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2024-08-24 22:14:15 -0700

    serve static assets

--- ejs/index.js
+++ ejs/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 
@@ -10,7 +13,12 @@ app.set('views', path.join(__dirname, '/views'));
 
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
-    res.render('subreddit', { subreddit });
+    const data = redditData[subreddit];
+    if (data) {
+        res.render('subreddit', { ...data });
+    } else {
+        res.render('notfound', { subreddit });
+    }
 });
 
 app.get('/cats', (req, res) => {
